794c65f2c459e277bcbebfc5b03a9f3d73f6eb60
moseskamira <moses.kamira@etinx.com>
2021-09-12 12:53:52 +0300

    Detail Page Impl

--- app/src/main/java/com/ndovu/myarticlesapp/views/ArticleDetailsActivity.kt
+++ app/src/main/java/com/ndovu/myarticlesapp/views/ArticleDetailsActivity.kt
@@ -2,29 +2,51 @@ package com.ndovu.myarticlesapp.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.widget.TextView
 import com.ndovu.myarticlesapp.R
 
 class ArticleDetailsActivity : AppCompatActivity() {
     private lateinit var articleDetailsBundle: Bundle
+    private lateinit var articleSourceTv: TextView
+    private lateinit var articleSectionTv: TextView
+    private lateinit var articleSubSectionTv: TextView
+    private lateinit var articleTitleTv: TextView
+    private lateinit var abstractTv: TextView
+    private lateinit var articleCreatedAtTv: TextView
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_article_details)
         articleDetailsBundle = intent.extras!!
+        articleSourceTv = findViewById(R.id.article_source_detail)
+        articleSectionTv = findViewById(R.id.article_section_detail)
+        articleSubSectionTv = findViewById(R.id.article_sub_section_detail)
+        articleTitleTv = findViewById(R.id.article_title_detail)
+        abstractTv = findViewById(R.id.article_abstract_detail)
+        articleCreatedAtTv = findViewById(R.id.article_created_at_detail)
         retrieveBundleData()
 
     }
 
 
     private fun retrieveBundleData() {
-        val type = articleDetailsBundle.getString("type", null)
-        val updatedAt = articleDetailsBundle.getString("updatedAt", null)
-        val source = articleDetailsBundle.getString("source", null)
-        val section = articleDetailsBundle.getString("section", null)
-        val subsection = articleDetailsBundle.getString("subsection", null)
-        if (type != null) {
-            Log.d("ARTICLESECTION", section)
-        }
+        val type = articleDetailsBundle.getString("type")!!
+        val updatedAt = articleDetailsBundle.getString("updatedAt")!!
+        val source = articleDetailsBundle.getString("source")!!
+        val section = articleDetailsBundle.getString("section")!!
+        val subsection = articleDetailsBundle.getString("subsection")!!
+        val title = articleDetailsBundle.getString("title")!!
+        val abstract = articleDetailsBundle.getString("abstract")!!
+        val createdAt = articleDetailsBundle.getString("createdAt")!!
+
+        articleTitleTv.text = title
+        abstractTv.text = abstract
+        articleCreatedAtTv.text = createdAt
+        articleSourceTv.text = source
+        articleSectionTv.text = section
+        articleSubSectionTv.text = subsection
 
 
     }
